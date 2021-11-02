44e48bf4f16732cd98392ea78fc5045492297bd5
thay-oliveira96 <thaynan-rodrigues@outlook.com>
2021-11-02 20:07:31 -0300

    Pausa no projeto

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/thay-oliveira96" target="_blank" rel="noreferrer">Thaynan Oliveira</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/Thaynan.Oliiveira" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
